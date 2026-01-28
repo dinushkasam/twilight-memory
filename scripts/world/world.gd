873d4f2a7fa477b469db54874d230392301e958c
dinushkasam <dinushkasam@gmail.com>
2026-01-28 20:32:04 +0530

    impl: basic world generation

--- scripts/world/world.gd
+++ scripts/world/world.gd
@@ -3,18 +3,24 @@ extends Node2D
 @onready var ground: TileMapLayer = $GroundTileMapLayer
 @onready var highlight: TileMapLayer = $GroundHighlightTileMapLayer
 
+@export var world_seed: NoiseTexture2D
+
 const BASE_BLOCKS_ID = 2
 
 const GRASS_BASE_ATLUS = Vector2i(0, 0)
+const GRASS_CUT_ATLUS = Vector2i(1, 0)
+const GRASS_1_ATLUS = Vector2i(2, 0)
 const HIGHLIGHT_ATLUS = Vector2i(6, 0)
 const SOIL_ATLUS = Vector2i(3, 0)
 const BOUNDARY_ATLUS = Vector2i(0, 1)
 
 var hovered_cell: Vector2i = Vector2i(-999, -999)
+@onready var world_height = world_seed.height * 1.0
+@onready var world_width = world_seed.width * 1.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	generate_test_map()
+	generate_world_map()
 	generate_boundaries()
 
 func _process(_delta: float) -> void:
@@ -24,15 +30,36 @@ func _input(event: InputEvent) -> void:
 	select_cell_mouse_click(event)
 
 
-func generate_test_map():
-	var size := 10.0
-	for x in range(-size/2, size/2):
-		for y in range(-size/2, size/2):
-			ground.set_cell(
-				Vector2i(x, y),
-				BASE_BLOCKS_ID,
-				GRASS_BASE_ATLUS
-			)
+func generate_world_map():
+	for x in range(-world_height/2, world_height/2):
+		for y in range(-world_width/2, world_width/2):
+			# noise value is between -0.5 and + 0.5
+			var noise = world_seed.noise.get_noise_2d(x, y)
+			
+			if noise >= 0.25:
+				ground.set_cell(
+					Vector2i(x, y),
+					BASE_BLOCKS_ID,
+					GRASS_BASE_ATLUS
+				)
+			elif noise >= 0:
+				ground.set_cell(
+					Vector2i(x, y),
+					BASE_BLOCKS_ID,
+					GRASS_CUT_ATLUS
+				)
+			elif noise >= -0.25:
+				ground.set_cell(
+					Vector2i(x, y),
+					BASE_BLOCKS_ID,
+					GRASS_1_ATLUS
+				)
+			else:
+				ground.set_cell(
+					Vector2i(x, y),
+					BASE_BLOCKS_ID,
+					SOIL_ATLUS
+				)
 
 
 func generate_boundaries():
