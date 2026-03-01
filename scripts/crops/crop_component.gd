0d4af05bd4347ddf00ce41c1142c8e8d08725cf6
¨Dinushka <dinushka.samaranayake@yaalalabs.com>
2026-03-01 17:46:59 +0530

    impl: crop growth based on a timer

--- scripts/crops/crop_component.gd
+++ scripts/crops/crop_component.gd
@@ -4,6 +4,11 @@ class_name CropComponent
 
 @export var data: CropData
 var current_stage = CropStage.SEED
+@export var sprite: AnimatedSprite2D
+
+# TEMP
+@export var timer: Timer
+
 
 enum CropStage {
 	SEED,
@@ -13,3 +18,35 @@ enum CropStage {
 	STALE,
 	HARVESTED
 }
+
+
+func update_animation():
+	sprite.play(data.animation)
+	sprite.set_frame_and_progress(current_stage, 0)
+	sprite.pause()
+
+func set_crop(data_arg: CropData):
+	data = data_arg
+	
+	# Start animation
+	update_animation()
+	start_stage_timer()
+
+func advance_stage():
+	var new_stage: int = current_stage + 1
+	current_stage = new_stage as CropStage
+	update_animation()
+	start_stage_timer()
+
+
+# TEMP
+func start_stage_timer():
+	if current_stage >= CropStage.RIPE:
+		return
+	
+	var duration = data.stage_durations.get(current_stage, 0)
+	if duration > 0:
+		timer.start(duration)
+
+func _on_timer_timeout() -> void:
+	advance_stage()
