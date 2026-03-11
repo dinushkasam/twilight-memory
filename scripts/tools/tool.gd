335f79d076399f12bd787ac884b3b5fb8ac842dd
¨Dinushka <dinushka.samaranayake@yaalalabs.com>
2026-03-11 16:19:41 +0530

    chore: implement seed box planting

--- scripts/tools/tool.gd
+++ scripts/tools/tool.gd
@@ -6,4 +6,4 @@ var data: ToolData
 
 
 func use(actor: Player, target_tile: Vector2i):
-	actor.world_context.interact(target_tile, actor, self)
+	WorldContext.interact(target_tile, actor, self)
--- scripts/tools/tool_data.gd
+++ scripts/tools/tool_data.gd
@@ -9,3 +9,6 @@ class_name ToolData
 @export var tool_power = 1
 @export var tool_range = 1.0
 @export var consumable = false
+
+@export var spawn_entity: PackedScene = null
+@export var spawn_args: Dictionary
